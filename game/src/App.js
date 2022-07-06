08b833e0b988caff2500b08158dcbe59f7fc5a4e
TarMuradyan99 <taronmuradyan1999@gmail.com>
2022-07-06 20:31:46 +0400

    created app with create-react-app

---
+++ game/src/App.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      balance:1000,
+    }
+  }
+  render(){
+    return(
+      <section>
+        
+      </section>
+    )
+  }
+}
+export default App
